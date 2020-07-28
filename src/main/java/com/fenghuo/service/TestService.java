2417a6f19e4ab283574070b21a348c696192d11d
15735394021 <liquan1998@qq.com>
2020-07-28 20:20:20 +0800

    SSM框架集成

--- src/main/java/com/fenghuo/service/TestService.java
+++ src/main/java/com/fenghuo/service/TestService.java
@@ -5,7 +5,6 @@ import com.fenghuo.domain.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface TestService {
     Test query();
 }
--- src/main/java/com/fenghuo/service/impl/TestServiceImpl.java
+++ src/main/java/com/fenghuo/service/impl/TestServiceImpl.java
@@ -4,7 +4,9 @@ import com.fenghuo.dao.TestDao;
 import com.fenghuo.domain.Test;
 import com.fenghuo.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TestServiceImpl implements TestService {
     @Autowired
     public TestDao testDao;
