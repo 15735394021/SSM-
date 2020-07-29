afcc2575f9f4de5d58df572e615afdaad398789f
15735394021 <liquan1998@qq.com>
2020-07-29 12:11:00 +0800

    SSM框架集成

--- src/main/java/com/fenghuo/service/TestService.java
+++ src/main/java/com/fenghuo/service/TestService.java
@@ -3,7 +3,6 @@ package com.fenghuo.service;
 import com.fenghuo.dao.TestDao;
 import com.fenghuo.domain.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 public interface TestService {
     Test query();
